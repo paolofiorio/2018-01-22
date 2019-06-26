1f05a343248983da3b5c094e3a921853e1e6905b
Paolo Fiorio <Paolo Fiorio@DESKTOP>
2019-06-26 09:11:13 +0200

    aggiornamento

--- src/it/polito/tdp/seriea/SerieAController.java
+++ src/it/polito/tdp/seriea/SerieAController.java
@@ -1,11 +1,15 @@
-/**
- * Sample Skeleton for 'SerieA.fxml' Controller Class
- */
-
 package it.polito.tdp.seriea;
 
 import java.net.URL;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.seriea.model.Model;
+import it.polito.tdp.seriea.model.Season;
+import it.polito.tdp.seriea.model.Team;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -13,44 +17,80 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 
 public class SerieAController {
+	
+	Model model ;
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // fx:id="boxSquadra"
-    private ChoiceBox<?> boxSquadra; // Value injected by FXMLLoader
+    @FXML
+    private ChoiceBox<Team> boxSquadra;
 
-    @FXML // fx:id="btnSelezionaSquadra"
-    private Button btnSelezionaSquadra; // Value injected by FXMLLoader
+    @FXML
+    private Button btnSelezionaSquadra;
 
-    @FXML // fx:id="btnTrovaAnnataOro"
-    private Button btnTrovaAnnataOro; // Value injected by FXMLLoader
+    @FXML
+    private Button btnTrovaAnnataOro;
 
-    @FXML // fx:id="btnTrovaCamminoVirtuoso"
-    private Button btnTrovaCamminoVirtuoso; // Value injected by FXMLLoader
+    @FXML
+    private Button btnTrovaCamminoVirtuoso;
 
-    @FXML // fx:id="txtResult"
-    private TextArea txtResult; // Value injected by FXMLLoader
+    @FXML
+    private TextArea txtResult;
 
     @FXML
     void doSelezionaSquadra(ActionEvent event) {
+    	Team t = boxSquadra.getValue() ;
+    	
+    	if(t==null) {
+    		txtResult.appendText("Devi selezionare una squadra\n");
+    		return ;
+    	}
+    	
+    	Map<Season, Integer> punteggi = model.calcolaPunteggi(t) ;
+    	
+    	txtResult.clear();
+    	
+    	for(Season s: punteggi.keySet()) {
+    		txtResult.appendText(String.format("%s: %d\n", s.getDescription(), punteggi.get(s)) );
+    	}
+    
+        btnTrovaAnnataOro.setDisable(false);
+        btnTrovaCamminoVirtuoso.setDisable(true);
 
     }
 
     @FXML
     void doTrovaAnnataOro(ActionEvent event) {
+    	
+    	Season annata = model.calcolaAnnataDOro() ;
+    	int deltaPesi = model.getDeltaPesi() ;
+    	txtResult.appendText(String.format("Annata d'oro: %s (differenza pesi %d)\n", annata.getDescription(), deltaPesi));
 
+        btnTrovaAnnataOro.setDisable(false);
+        btnTrovaCamminoVirtuoso.setDisable(false);
     }
 
     @FXML
     void doTrovaCamminoVirtuoso(ActionEvent event) {
+    	List<Season> percorso = model.camminoVirtuoso() ;
+    	for(Season s: percorso) {
+    		txtResult.appendText(String.format("%s: %d\n", s.getDescription(), model.getPunteggio(s)));
+    	}
 
     }
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
+    public void setModel(Model m) {
+    	this.model = m ;
+    	
+    	boxSquadra.getItems().clear();
+    	boxSquadra.getItems().addAll(model.getSquadre()) ;
+    }
+    
+    @FXML
     void initialize() {
         assert boxSquadra != null : "fx:id=\"boxSquadra\" was not injected: check your FXML file 'SerieA.fxml'.";
         assert btnSelezionaSquadra != null : "fx:id=\"btnSelezionaSquadra\" was not injected: check your FXML file 'SerieA.fxml'.";
@@ -58,5 +98,18 @@ public class SerieAController {
         assert btnTrovaCamminoVirtuoso != null : "fx:id=\"btnTrovaCamminoVirtuoso\" was not injected: check your FXML file 'SerieA.fxml'.";
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'SerieA.fxml'.";
 
+        // i bottoni Annata d'oro e Cammino Virtuoso si abilitano solo dopo avere scelto la squadra
+        btnTrovaAnnataOro.setDisable(true);
+        btnTrovaCamminoVirtuoso.setDisable(true);
+
+        // disable buttons when team is changed
+        // https://stackoverflow.com/a/35282753/986709
+        boxSquadra.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Team>() {
+			@Override
+			public void changed(ObservableValue<? extends Team> observable, Team oldValue, Team newValue) {
+		        btnTrovaAnnataOro.setDisable(true);
+		        btnTrovaCamminoVirtuoso.setDisable(true);				
+			}
+		});
     }
 }
--- src/it/polito/tdp/seriea/model/Model.java
+++ src/it/polito/tdp/seriea/model/Model.java
@@ -1,5 +1,219 @@
 package it.polito.tdp.seriea.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleDirectedWeightedGraph;
+
+import it.polito.tdp.seriea.db.SerieADAO;
+
 public class Model {
+
+	private List<Team> squadre;
+	private List<Season> stagioni;
+	private Map<String, Team> squadreIdMap;
+	private Map<Integer, Season> stagioniIdMap;
+
+	private Team squadraSelezionata;
+	private Map<Season, Integer> punteggi;
+	private List<Season> stagioniConsecutive;
+
+	private Graph<Season, DefaultWeightedEdge> grafo;
+
+	private List<Season> percorsoBest;
+	private int deltaPesi;
+
+	public Model() {
+		SerieADAO dao = new SerieADAO();
+
+		this.squadre = dao.listTeams();
+		this.squadreIdMap = new HashMap<String, Team>();
+		for (Team t : this.squadre) {
+			this.squadreIdMap.put(t.getTeam(), t);
+		}
+
+		this.stagioni = dao.listAllSeasons();
+		this.stagioniIdMap = new HashMap<Integer, Season>();
+		for (Season s : this.stagioni) {
+			this.stagioniIdMap.put(s.getSeason(), s);
+		}
+
+	}
+
+	public List<Team> getSquadre() {
+		return this.squadre;
+	}
+
+	public Map<Season, Integer> calcolaPunteggi(Team squadra) {
+
+		this.squadraSelezionata = squadra;
+
+		this.punteggi = new HashMap<Season, Integer>();
+
+		SerieADAO dao = new SerieADAO();
+
+		List<Match> partite = dao.listMatchesForTeam(squadra, stagioniIdMap, squadreIdMap);
+
+		for (Match m : partite) {
+
+			Season stagione = m.getSeason();
+
+			int punti = 0;
+
+			if (m.getFtr().equals("D")) {
+				punti = 1;
+			} else {
+				if ((m.getHomeTeam().equals(squadra) && m.getFtr().equals("H"))
+						|| (m.getAwayTeam().equals(squadra) && m.getFtr().equals("A"))) {
+					punti = 3;
+				}
+			}
+
+			Integer attuale = punteggi.get(stagione);
+			if (attuale == null)
+				attuale = 0;
+			punteggi.put(stagione, attuale + punti);
+
+		}
+
+		return punteggi;
+
+	}
+
+	public Season calcolaAnnataDOro() {
+
+		// costruisco il grafo
+		this.grafo = new SimpleDirectedWeightedGraph<Season, DefaultWeightedEdge>(DefaultWeightedEdge.class);
+		Graphs.addAllVertices(this.grafo, punteggi.keySet());
+
+		for (Season s1 : punteggi.keySet()) {
+			for (Season s2 : punteggi.keySet()) {
+				if (!s1.equals(s2)) {
+					int punti1 = punteggi.get(s1);
+					int punti2 = punteggi.get(s2);
+					if (punti1 > punti2) {
+						Graphs.addEdge(this.grafo, s2, s1, punti1 - punti2);
+					} else {
+						Graphs.addEdge(this.grafo, s1, s2, punti2 - punti1);
+					}
+				}
+			}
+		}
+
+		if (grafo.vertexSet().isEmpty()) {
+			// caso particolare: nessuna stagione
+			this.deltaPesi = 0;
+			return null;
+		} else if (grafo.vertexSet().size() == 1) {
+			// caso particolare: se c'è solo una stagione
+			this.deltaPesi = 0;
+			/*
+			 * ugly trick to extract the only element in the set. See:
+			 * <https://stackoverflow.com/questions/5229137/the-correct-way-to-return-the-only-element-from-a-set>
+			 */
+			Season unica = grafo.vertexSet().iterator().next();
+			return unica;
+		} else {
+
+			// trovo l'annata migliore
+			Season migliore = null;
+			int max = 0;
+			for (Season s : grafo.vertexSet()) {
+				int valore = pesoStagione(s);
+				if (valore > max) {
+					max = valore;
+					migliore = s;
+				}
+			}
+			this.deltaPesi = max;
+			return migliore;
+		}
+
+	}
+
+	public int getDeltaPesi() {
+		return this.deltaPesi;
+	}
+
+	private int pesoStagione(Season s) {
+		int somma = 0;
+
+		for (DefaultWeightedEdge e : grafo.incomingEdgesOf(s)) {
+			somma = somma + (int) grafo.getEdgeWeight(e);
+		}
+
+		for (DefaultWeightedEdge e : grafo.outgoingEdgesOf(s)) {
+			somma = somma - (int) grafo.getEdgeWeight(e);
+		}
+
+		return somma;
+	}
+
+	public int getPunteggio(Season s) {
+		return this.punteggi.get(s) ;
+	}
+	
+	public List<Season> camminoVirtuoso() {
+
+		// trova le stagioni consecutive
+		this.stagioniConsecutive = new ArrayList<Season>(punteggi.keySet());
+		Collections.sort(this.stagioniConsecutive);
+
+		// prepara le variabili utili alla ricorsione
+		List<Season> parziale = new ArrayList<Season>();
+		this.percorsoBest = new ArrayList<>();
+
+		// Itera al livello zero
+		for (Season s : grafo.vertexSet()) {
+			parziale.add(s);
+			cerca(1, parziale);
+			parziale.remove(0);
+		}
+
+		return percorsoBest;
+	}
+
+	/*
+	 * RICORSIONE
+	 * 
+	 * Soluzione parziale: Lista di Season (lista di vertici) Livello ricorsione:
+	 * lunghezza della lista Casi terminali: non trova altri vertici da aggiungere
+	 * -> verifica se il cammino ha lunghezza massima tra quelli visti finora
+	 * Generazione delle soluzioni: vertici connessi all'ultimo vertice del percorso
+	 * (con arco orientato nel verso giusto), non ancora parte del percorso,
+	 * relativi a stagioni consecutive.
+	 */
+
+	private void cerca(int livello, List<Season> parziale) {
+		boolean trovato = false;
+
+		// genera nuove soluzioni
+		Season ultimo = parziale.get(livello - 1);
+
+		for (Season prossimo : Graphs.successorListOf(grafo, ultimo)) {
+			if (!parziale.contains(prossimo)) {
+				if (stagioniConsecutive.indexOf(ultimo) + 1 == stagioniConsecutive.lastIndexOf(prossimo)) {
+					// candidato accettabile -> fai ricorsione
+					trovato = true;
+					parziale.add(prossimo);
+					cerca(livello + 1, parziale);
+					parziale.remove(livello);
+				}
+			}
+		}
+
+		// valuta caso terminale
+		if (!trovato) {
+			if (parziale.size() > percorsoBest.size()) {
+				percorsoBest = new ArrayList<Season>(parziale); // clona il best
+			}
+		}
+	}
 
 }
